2fc71b3ed2dc8708d0c8ebc23eaf9e7fbe4774ac
SaltSpectre <200460916+SaltSpectre@users.noreply.github.com>
2026-04-29 14:53:27 -0400

    style(cs): format code for consistency and readability

--- mouse.cs
+++ mouse.cs
@@ -3,21 +3,25 @@
 
 using System;
 using System.Runtime.InteropServices;
-public static class Mouse {
+
+public static class Mouse
+{
     [StructLayout(LayoutKind.Sequential)]
-    public struct INPUT {
-    public int type;
-    public MOUSEINPUT mi;
+    public struct INPUT
+    {
+        public int type;
+        public MOUSEINPUT mi;
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct MOUSEINPUT {
-    public int dx;
-    public int dy;
-    public uint mouseData;
-    public uint dwFlags;
-    public uint time;
-    public IntPtr dwExtraInfo;
+    public struct MOUSEINPUT
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
     }
 
     [DllImport("user32.dll")]
@@ -30,9 +34,10 @@ public static class Mouse {
     public static extern int GetSystemMetrics(int nIndex);
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct POINT {
-    public int x;
-    public int y;
+    public struct POINT
+    {
+        public int x;
+        public int y;
     }
 
     const int INPUT_MOUSE = 0;
@@ -41,18 +46,19 @@ public static class Mouse {
     const int SM_CXSCREEN = 0;
     const int SM_CYSCREEN = 1;
 
-    public static void MoveMouse() {
-    INPUT Input = new INPUT();
-    POINT CurrentPosition;
+    public static void MoveMouse()
+    {
+        INPUT Input = new INPUT();
+        POINT CurrentPosition;
 
-    GetCursorPos(out CurrentPosition);
+        GetCursorPos(out CurrentPosition);
 
-    Input.type = INPUT_MOUSE;
-    Input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+        Input.type = INPUT_MOUSE;
+        Input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
 
-    Input.mi.dx = (int)(CurrentPosition.x * (65536.0f / GetSystemMetrics(SM_CXSCREEN)));
-    Input.mi.dy = (int)(CurrentPosition.y * (65536.0f / GetSystemMetrics(SM_CYSCREEN)));
+        Input.mi.dx = (int)(CurrentPosition.x * (65536.0f / GetSystemMetrics(SM_CXSCREEN)));
+        Input.mi.dy = (int)(CurrentPosition.y * (65536.0f / GetSystemMetrics(SM_CYSCREEN)));
 
-    SendInput(1, new INPUT[] { Input }, Marshal.SizeOf(typeof(INPUT)));
+        SendInput(1, new INPUT[] { Input }, Marshal.SizeOf(typeof(INPUT)));
     }
 }
--- user32.cs
+++ user32.cs
@@ -2,41 +2,70 @@
 
 using System;
 using System.Runtime.InteropServices;
-public static class User32 {
+
+public static class User32
+{
     public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
     public delegate bool EnumChildProc(IntPtr hWnd, IntPtr lParam);
+
     [DllImport("user32.dll")]
     public static extern bool EnumWindows(EnumWindowsProc enumFunc, IntPtr lParam);
+
     [DllImport("user32.dll")]
-    public static extern bool EnumChildWindows(IntPtr hWndParent, EnumChildProc lpEnumFunc, IntPtr lParam);
+    public static extern bool EnumChildWindows(
+        IntPtr hWndParent,
+        EnumChildProc lpEnumFunc,
+        IntPtr lParam
+    );
+
     [DllImport("user32.dll")]
-    public static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+    public static extern int GetWindowText(
+        IntPtr hWnd,
+        System.Text.StringBuilder lpString,
+        int nMaxCount
+    );
+
     [DllImport("user32.dll")]
-    public static extern int GetClassName(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+    public static extern int GetClassName(
+        IntPtr hWnd,
+        System.Text.StringBuilder lpString,
+        int nMaxCount
+    );
+
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
+
     [DllImport("user32.dll")]
     public static extern bool SetForegroundWindow(IntPtr hWnd);
+
     [DllImport("user32.dll", SetLastError = true)]
     public static extern IntPtr CreateWindowEx(
         uint dwExStyle,
         string lpClassName,
         string lpWindowName,
         uint dwStyle,
-        int x, int y,
-        int nWidth, int nHeight,
+        int x,
+        int y,
+        int nWidth,
+        int nHeight,
         IntPtr hWndParent,
         IntPtr hMenu,
         IntPtr hInstance,
-        IntPtr lpParam);
+        IntPtr lpParam
+    );
+
     [DllImport("user32.dll")]
     public static extern bool DestroyWindow(IntPtr hWnd);
+
     [DllImport("kernel32.dll")]
     public static extern IntPtr GetModuleHandle(string lpModuleName);
+
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
     [DllImport("user32.dll")]
     public static extern bool IsIconic(IntPtr hWnd);
+
     [DllImport("user32.dll")]
     public static extern IntPtr GetAncestor(IntPtr hWnd, uint gaFlags);
 }
